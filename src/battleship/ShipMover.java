d8c0e8c0d52b448df0431ce211059257da8ecc30
Steve R <svramusi@gmail.com>
2013-03-05 19:40:20 -0500

    refactoring

--- src/battleship/ShipMover.java
+++ src/battleship/ShipMover.java
@@ -9,20 +9,10 @@ import board.Board;
 
 import java.util.*;
 
-public class ShipMover {
-	
-	ShipType shipType;
-	Point target;
-	Board board;
-	
-	public ShipMover(ShipType shipType, Point target, Board board)
-	{
-		this.shipType = shipType;
-		this.target = target;
-		this.board = board;
-	}
+public class ShipMover 
+{
 
-	private int getMoveDistance(int maxMoveDistance, int distanceNeeded)
+	private static int getMoveDistance(int maxMoveDistance, int distanceNeeded)
 	{
 		if(distanceNeeded > maxMoveDistance)
 			return maxMoveDistance;
@@ -33,7 +23,92 @@ public class ShipMover {
 	//NEED TO MAKE THIS A UTILITY FUNCION
 	//NEED TO EVALUATE ALL OPTIONS AND GO FROM THERE
 	
-	public void moveShip()
+	private static Point getBestHorizontalMovement(Ship ship, Point origStartingPoint, Point target)
+	{
+		Point closestPoint = GetClosestPoint(target, ship.getShipLocation());
+		int yDistance = BattleshipUtils.getYDistance(closestPoint, target);
+
+		int distanceToMove = getMoveDistance(ship.getMoveDistance(), yDistance);
+
+		if(origStartingPoint.getY() < target.getY())
+			return new Point(origStartingPoint.getX(), origStartingPoint.getY() + distanceToMove);
+		else
+			return new Point(origStartingPoint.getX(), origStartingPoint.getY() - distanceToMove);
+	}
+	
+	private static Point getBestVerticalMovement(Ship ship, Point origStartingPoint, Point target)
+	{
+		Point closestPoint = GetClosestPoint(target, ship.getShipLocation());
+		int xDistance = BattleshipUtils.getXDistance(closestPoint, target);
+		
+		int distanceToMove = getMoveDistance(ship.getMoveDistance(), xDistance);
+
+		if(origStartingPoint.getX() < target.getX())
+			return new Point(origStartingPoint.getX() + distanceToMove, origStartingPoint.getY());
+		else
+			return new Point(origStartingPoint.getX() - distanceToMove, origStartingPoint.getY());
+	}
+	
+	private static void utility(ShipType shipType, Point target, Board board)
+	{
+		Ship ship = board.getShip(shipType);
+
+		if(ship.isValidShot(target))
+			return;
+	
+	
+		Point origStartingPoint = ship.getStartPoint();
+		Point newStartingPoint = origStartingPoint;
+		
+		Point closestPoint = GetClosestPoint(target, ship.getShipLocation());
+		
+		int xDistance = BattleshipUtils.getXDistance(closestPoint, target);
+		int yDistance = BattleshipUtils.getYDistance(closestPoint, target);
+		
+		Direction direction = ship.getDirection();
+
+		int distanceToMove;
+		if((direction == Direction.UP || direction == Direction.DOWN) && xDistance > ship.getShootDistance())
+		{
+			distanceToMove = getMoveDistance(ship.getMoveDistance(), xDistance);
+
+			if(origStartingPoint.getX() < target.getX())
+				newStartingPoint = new Point(origStartingPoint.getX() + distanceToMove, origStartingPoint.getY());
+			else
+				newStartingPoint = new Point(origStartingPoint.getX() - distanceToMove, origStartingPoint.getY());
+		}
+		else if((direction == Direction.RIGHT || direction == Direction.LEFT) && yDistance > ship.getShootDistance())
+		{
+			distanceToMove = getMoveDistance(ship.getMoveDistance(), yDistance);
+
+			if(origStartingPoint.getY() < target.getY())
+				newStartingPoint = new Point(origStartingPoint.getX(), origStartingPoint.getY() + distanceToMove);
+			else
+				newStartingPoint = new Point(origStartingPoint.getX(), origStartingPoint.getY() - distanceToMove);
+		}
+		else
+		{
+			if(xDistance > ship.getShootDistance() || yDistance > ship.getShootDistance())
+			{
+				if(direction == Direction.UP || direction == Direction.DOWN)
+					direction = getNewDirection(ship, newStartingPoint, direction, Direction.RIGHT, Direction.LEFT);
+				else //(direction == Direction.RIGHT || direction == Direction.LEFT)
+					direction = getNewDirection(ship, newStartingPoint, direction, Direction.UP, Direction.DOWN);
+			}
+		}
+		
+		
+	}
+
+	private static Point GetClosestPoint(Point target, List<Point> shipLocation)
+	{
+		Point closestXPoint = BattleshipUtils.getClosestXPoint(shipLocation, target);
+		Point closestYPoint = BattleshipUtils.getClosestYPoint(shipLocation, target);
+		
+		return new Point(closestXPoint.getX(), closestYPoint.getY());
+	}
+	
+	public static void moveShip(ShipType shipType, Point target, Board board)
 	{
 		Ship ship = board.getShip(shipType);
 		
@@ -42,10 +117,7 @@ public class ShipMover {
 			Point origStartingPoint = ship.getStartPoint();
 			Point newStartingPoint = origStartingPoint;
 			
-			Point closestXPoint = BattleshipUtils.getClosestXPoint(ship.getShipLocation(), target);
-			Point closestYPoint = BattleshipUtils.getClosestYPoint(ship.getShipLocation(), target);
-			
-			Point closestPoint = new Point(closestXPoint.getX(), closestYPoint.getY());
+			Point closestPoint = GetClosestPoint(target, ship);
 			
 			int xDistance = BattleshipUtils.getXDistance(closestPoint, target);
 			int yDistance = BattleshipUtils.getYDistance(closestPoint, target);
@@ -99,7 +171,7 @@ public class ShipMover {
 		}
 	}
 
-	private Direction getNewDirection(Ship ship, Point newStartingPoint, 
+	private static Direction getNewDirection(Ship ship, Point newStartingPoint, 
 			Direction origDirection, Direction option1, Direction option2) 
 	{
 		int option1Dist = tryDifferentDirection(ship, newStartingPoint, option1, origDirection);
